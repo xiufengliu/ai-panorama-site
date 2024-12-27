f7a85a960e3d34bc49ffd447c78f3ea04cb296ea
xiufengliu <groupme@gmail.com>
2024-12-27 19:25:23 +0100

    update

--- app.py
+++ app.py
@@ -1,36 +1,43 @@
 import streamlit as st
 from views import admin, book
 
-st.set_page_config(page_title="AI全景探索", layout="wide", initial_sidebar_state="collapsed")
+def apply_custom_css():
+    st.markdown("""
+    <style>
+        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+            width: 300px;
+        }
+        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+            width: 0px;
+            margin-left: -300px;
+        }
+        [data-testid="stVerticalBlock"] {
+            padding-left: 0rem;
+            padding-right: 0rem;
+        }
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+    </style>
+    """, unsafe_allow_html=True)
 
-# Custom CSS
-st.markdown("""
-<style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        width: 300px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        width: 0px;
-        margin-left: -300px;
-    }
-    [data-testid="stVerticalBlock"] {
-        padding-left: 0rem;
-        padding-right: 0rem;
-    }
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-</style>
-""", unsafe_allow_html=True)
-
-# Sidebar navigation
-st.sidebar.title("导航")
-selection = st.sidebar.radio("前往", ["书籍", "管理"])
-
-# Display selected view
-if selection == "书籍":
-    book.show()
-elif selection == "管理":
-    admin.show()
+def main():
+    # Apply custom CSS
+    apply_custom_css()
+    
+    # Sidebar navigation
+    st.sidebar.title("导航")
+    selection = st.sidebar.radio("前往", ["书籍", "管理"])
+    
+    # Display selected view
+    if selection == "书籍":
+        book.show()
+    elif selection == "管理":
+        admin.show()
 
 if __name__ == "__main__":
-    pass
+    st.set_page_config(
+        page_title="AI全景探索",
+        layout="wide",
+        initial_sidebar_state="collapsed"
+    )
+    main()
